f35ea1c1e93dff7060c49ec9a4cff52f1da1f750
MachariaP <walburphinehas78@gmail.com>
2024-10-21 12:20:10 +0300

    Created views and urls in the app and configured the urls settings and the apps settings

--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -1,17 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-
-def home(request):
-    return HttpResponse('<h1>Home</h1>')
-
-def room(request):
-    return HttpResponse('<h1>Room</h1>')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('room/', room),
+    path('', include('base.urls')),  # Include base app's URL configuration
 ]
