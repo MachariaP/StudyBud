7430825f59921f6d9157a1f7f6982973c58bd184
MachariaP <walburphinehas78@gmail.com>
2024-10-24 15:35:35 +0300

    Implemented room deletion URL with deleteRoom view

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('create-room/', views.createRoom, name='create-room'),  # Add URL for creating a new room.
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),  # Add URL for updating a room.
+    path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),  # Add URL for updating a room.
+
 ]
