28a41a4924cbae4f3a1a24a4d23778aacd1fdf2f
MachariaP <walburphinehas78@gmail.com>
2024-10-24 11:53:15 +0300

    Implemented room creation URL with 'createRoom' view

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('login/', views.login, name='login'),
+    path('create-room/', views.createRoom, name='create-room'),  # Add URL for creating a new room.
 ]
