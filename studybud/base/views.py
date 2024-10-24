5696fc31875ee556ef3a75a053c0dd991f2c162a
MachariaP <walburphinehas78@gmail.com>
2024-10-24 11:48:37 +0300

    Implemented POST request handling with RoomForm

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Room
+from .forms import RoomForm
 
 
 # Create your views here.
@@ -20,6 +21,19 @@ def room(request, pk):
     context = {'room': room}        
 
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context = {'form': form}
+
+    return render(request, 'base/room_form.html', context)
 
 def login(request):
     return render(request, 'login.html')
