a084a3cd1ec35d3d7c087e624ea5d3ef567deaf5
MachariaP <walburphinehas78@gmail.com>
2024-10-24 12:32:55 +0300

    Implemented updateRoom function for editing rooms

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -35,5 +35,13 @@ def createRoom(request):
 
     return render(request, 'base/room_form.html', context)
 
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
 def login(request):
     return render(request, 'login.html')
