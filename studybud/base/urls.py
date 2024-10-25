43c554253bca3df5612553a7ef06f2826f1b8ded
MachariaP <walburphinehas78@gmail.com>
2024-10-25 18:47:36 +0300

    Updated login URL path to reference loginPage view and reordered paths for improved readability.

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),  # Add URL for logging in/out. This is just a placeholder. You should add a proper login view.
+
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
-    path('login/', views.login, name='login'),
+    
     path('create-room/', views.createRoom, name='create-room'),  # Add URL for creating a new room.
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),  # Add URL for updating a room.
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),  # Add URL for updating a room.
