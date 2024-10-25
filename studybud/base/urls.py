59ad5870e606b9d12a2eba1cfc7dfbd90500b04c
MachariaP <walburphinehas78@gmail.com>
2024-10-25 20:31:59 +0300

    Add logout URL path for user logout functionality in URL patterns.

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.loginPage, name='login'),  # Add URL for logging in/out. This is just a placeholder. You should add a proper login view.
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),  # Add URL for logging out a user.
 
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
-    
+
     path('create-room/', views.createRoom, name='create-room'),  # Add URL for creating a new room.
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),  # Add URL for updating a room.
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),  # Add URL for updating a room.
