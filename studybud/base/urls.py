a4bae431e48d3d1ea4b6873da0185d5ae4ac579b
MachariaP <walburphinehas78@gmail.com>
2024-11-06 17:20:45 +0300

    Add URL paths for user and room management
    - Added URL path for updating user profile ().
    - Corrected comment for  and  paths.
    - Maintained consistent routing for login, registration, room, and message management.

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
@@ -13,8 +12,8 @@ urlpatterns = [
 
     path('create-room/', views.createRoom, name='create-room'),  # Add URL for creating a new room.
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),  # Add URL for updating a room.
-    path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),  # Add URL for updating a room.
-    path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message'),  # Add URL for updating a room.
+    path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),  # Add URL for deleting a room.
+    path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message'),  # Add URL for deleting a message.
 
-
+    path('update-user/', views.updateUser, name='update-user'),  # Add URL for updating a user.
 ]
