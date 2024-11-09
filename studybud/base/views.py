d6cfadcd1f7a20034d19a15e3ad276b8eeec716b
MachariaP <walburphinehas78@gmail.com>
2024-11-09 09:58:43 +0300

    Add topics page view and integrate topic filtering
    - Implemented 'topicsPage' view to display and filter topics based on search query.
    - Updated 'home' view to limit displayed topics to top 5 for streamlined UI.
    - Improved topic browsing by adding a dedicated topics page.

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -66,7 +66,7 @@ def home(request):
         Q(description__icontains=q)
     )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -177,3 +177,9 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form': form})
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
